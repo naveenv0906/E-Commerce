a721db92e1638a1006fb9b3616611d141c84fa5e
naveenv0906 <naveenv0906@gmail.com>
2024-07-22 16:04:09 +0530

    config added

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const dotenv = require('dotenv');
-
-dotenv.config();
+const db = require('./config/db')
 
 const productRoutes = require('./routes/product');
 
@@ -18,13 +17,6 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
-// Use routes
 app.use('/products', productRoutes);
 
 app.listen(port, () => {
