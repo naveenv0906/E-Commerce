e30e4a6a6cdc24b1e17e93b372ed5295e190944f
naveenv0906 <naveenv0906@gmail.com>
2024-07-23 17:14:15 +0530

    commit problem solved

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const db = require('./config/db')
-
-const productRoutes = require('./routes/product');
+const middleware = require('./middleware/middleware');
+const db = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.static(path.join(__dirname, 'views')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/products', productRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
